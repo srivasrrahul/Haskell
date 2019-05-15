0a058997fdadfa873df9a1497fea1d99123b5591
srivasrrahul <srivasrrahul@gmail.com>
2019-05-15 19:16:03 +0530

    adder

--- interactive.hs
+++ interactive.hs
@@ -1,4 +1,4 @@
-
+import Data.Char
 get_my_line :: IO String
 get_my_line = do 
 	x <- getChar
@@ -24,4 +24,34 @@ put_my_line (x:xs) = do
 my_strlen :: String -> IO ()
 my_strlen x = do
 	put_my_line (show (length x))
-           
+
+
+prompt :: String -> IO String
+prompt x = do
+	putStrLn x
+	n <- getLine
+	return n
+
+
+prompt_till_n :: String -> Int -> Int -> IO ()
+prompt_till_n x 1 sum_collected = do
+	putStrLn x
+	l <- getLine
+	let d = (read l :: Int)
+	putStrLn (show (d+sum_collected))
+
+
+prompt_till_n x n sum_collected = do
+	putStrLn x
+	l <- getLine
+	let d = (read l :: Int)
+	prompt_till_n x (n-1) (sum_collected + d)
+	
+
+adder :: IO ()
+adder = do
+	n <- prompt "How many numbers?"
+	let d = (read n :: Int)
+	prompt_till_n "Enter number :" d 0
+ 
+	
